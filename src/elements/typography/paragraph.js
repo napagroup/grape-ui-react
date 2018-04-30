38db7ec782f67ba60b89bd85c3c6e217019d99f1
ryanespin <ryanespin@gmail.com>
2018-04-30 12:43:07 -0400

    Initial styling for text.

---
+++ src/elements/typography/paragraph.js
@@ -0,0 +1,14 @@
+import { css } from 'styled-components';
+import { native as fontFamilyNative } from 'assets/json/fontFamily';
+import { base as fontSizeBase } from 'assets/json/fontSize';
+
+const baseFontFamily = fontFamilyNative;
+const baseFontSize = fontSizeBase;
+
+const textStylesBase = css`
+  font-family: ${baseFontFamily};
+  font-size: ${baseFontSize};
+  line-height: 1.5;
+`;
+
+export default textStylesBase;
