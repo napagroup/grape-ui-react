9c9b44f057c62e47be58183989482a349d1b5e9f
NAPA\respin <respin@napa.com>
2018-05-03 15:25:40 -0400

    Redoing textStylesBase.

--- src/elements/typography/textStyles.js
+++ src/elements/typography/textStyles.js
@@ -2,31 +2,20 @@ import PropTypes from 'prop-types';
 import { css } from 'styled-components';
 
 export const textStylesBase = props => {
-  const { italic, lg, sm, } = props;
-  let fontSize = '100%';
-  if (sm) {
-    fontSize = '80%';
-  }
-  else if (lg) {
-    fontSize = '120%';
-  }
+  const { italic, } = props;
+  const baseFontFamily = fontFamilyNative;
+  const baseFontSize = fontSizeBase;
   const Primitive = css`
-    font-size: ${fontSize};
     line-height: 1.5;
-    ${italic ? 'font-style: italic;' : ''}
+    ${italic ? 'text-style: italic;' : ''}
   `;
   return Primitive;
 };
 
 textStylesBase.propTypes = {
   italic: PropTypes.bool,
-  lg: PropTypes.bool,
-  sm: PropTypes.bool,
 };
 
 textStylesBase.defaultProps = {
   italic: false,
-  lg: false,
-  sm: false,
 };
-
