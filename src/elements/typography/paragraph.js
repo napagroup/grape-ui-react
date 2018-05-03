282b6237fca7bbb53adfa8c63385ea14e0aacff6
NAPA\respin <respin@napa.com>
2018-05-03 15:51:09 -0400

    defining italic.

--- src/elements/typography/paragraph.js
+++ src/elements/typography/paragraph.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import textStylesBase from './textStyles';
+import { textStylesBase } from './textStyles';
 
 export const Paragraph = styled.p`
   ${textStylesBase}
--- src/elements/typography/textStyles.js
+++ src/elements/typography/textStyles.js
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types';
 import { css } from 'styled-components';
 
-export const textStylesBase = css`
-  line-height: 1.5;
-  ${italic ? 'text-style: italic;' : ''}
-`;
+export const textStylesBase = props => {
+  const { italic, } = props;
+  const Primitive = css`
+    line-height: 1.5;
+    ${italic ? 'text-style: italic;' : ''}
+  `;
+  return Primitive;
+};
 
 textStylesBase.propTypes = {
   italic: PropTypes.bool,
