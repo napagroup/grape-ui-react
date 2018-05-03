8f2a5fb7fce040002b21d610ea97fc56228e157e
NAPA\respin <respin@napa.com>
2018-05-03 15:57:01 -0400

    Testing Italic again.

--- src/elements/typography/textStyles.js
+++ src/elements/typography/textStyles.js
@@ -1,19 +1,7 @@
 import PropTypes from 'prop-types';
 import { css } from 'styled-components';
 
-export const textStylesBase = props => {
-  const { italic, } = props;
-  const Primitive = css`
-    line-height: 1.5;
-    ${italic ? 'text-style: italic;' : ''}
-  `;
-  return Primitive;
-};
-
-textStylesBase.propTypes = {
-  italic: PropTypes.bool,
-};
-
-textStylesBase.defaultProps = {
-  italic: false,
-};
+export const textStylesBase = css`
+  line-height: 1.5;
+  ${props => props.italic ? 'text-style: italic;' : ''}
+`;
