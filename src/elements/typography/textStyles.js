d5b2d30c2ce7ebfb850a5d571841689d712b0caf
NAPA\respin <respin@napa.com>
2018-05-03 15:47:50 -0400

    Fixed import.

--- src/elements/typography/textStyles.js
+++ src/elements/typography/textStyles.js
@@ -1,6 +1,15 @@
+import PropTypes from 'prop-types';
 import { css } from 'styled-components';
 
 export const textStylesBase = css`
   line-height: 1.5;
-  ${props => props.italic ? 'text-style: italic;' : ''}
+  ${italic ? 'text-style: italic;' : ''}
 `;
+
+textStylesBase.propTypes = {
+  italic: PropTypes.bool,
+};
+
+textStylesBase.defaultProps = {
+  italic: false,
+};
