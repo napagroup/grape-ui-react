d017c60bd84d35f951afd877aa02d29abef2e09e
NAPA\respin <respin@napa.com>
2018-05-03 17:15:23 -0400

    Added in Header.

--- src/elements/typography/Header.js
+++ src/elements/typography/Header.js
@@ -1,7 +1,18 @@
 import styled from 'styled-components';
 import { textStylesBase } from './textStyles';
+import * as fontSize from '../../assets/json/fontSize.json';
 
-export const Header = styled.h1`
+const Header = styled.h1`
   ${textStylesBase}
+  font-size: ${fontSize.h1};
 `;
+
+Header.h1 = Header;
+Header.h2 = Header.withComponent('h2').extend`font-size: ${fontSize.h2};`;
+Header.h3 = Header.withComponent('h3').extend`font-size: ${fontSize.h3};`;
+Header.h4 = Header.withComponent('h4').extend`font-size: ${fontSize.h4};`;
+Header.h5 = Header.withComponent('h5').extend`font-size: ${fontSize.h5};`;
+Header.h6 = Header.withComponent('h6').extend`font-size: ${fontSize.h6};`;
+
+export { Header };
 
