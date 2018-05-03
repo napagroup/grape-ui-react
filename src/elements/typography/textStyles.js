be3019b56ee0578461987761e68a999a6515ff59
NAPA\respin <respin@napa.com>
2018-05-03 16:09:08 -0400

    Testing out with propTypes again.

--- src/elements/typography/textStyles.js
+++ src/elements/typography/textStyles.js
@@ -1,7 +1,19 @@
 import PropTypes from 'prop-types';
 import { css } from 'styled-components';
 
-export const textStylesBase = css`
-  line-height: 1.5;
-  ${props => props.italic ? 'font-style: italic;' : ''}
-`;
+export const textStylesBase = props => {
+  const { italic, } = props;
+  const Primitive = css`
+    line-height: 1.5;
+    ${italic ? 'font-style: italic;' : ''}
+  `;
+  return Primitive;
+};
+
+textStylesBase.propTypes = {
+  italic: PropTypes.bool,
+};
+
+textStylesBase.defaultProps = {
+  italic: false,
+};
