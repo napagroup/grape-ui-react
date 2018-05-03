d9baa22af7f736f8f0d6b9ac80fa4fe228bb4ec0
NAPA\respin <respin@napa.com>
2018-05-03 15:41:46 -0400

    Passing in props different way.

--- src/elements/typography/textStyles.js
+++ src/elements/typography/textStyles.js
@@ -1,21 +1,6 @@
-import PropTypes from 'prop-types';
 import { css } from 'styled-components';
 
-export const textStylesBase = props => {
-  const { italic, } = props;
-  const baseFontFamily = fontFamilyNative;
-  const baseFontSize = fontSizeBase;
-  const Primitive = css`
-    line-height: 1.5;
-    ${italic ? 'text-style: italic;' : ''}
-  `;
-  return Primitive;
-};
-
-textStylesBase.propTypes = {
-  italic: PropTypes.bool,
-};
-
-textStylesBase.defaultProps = {
-  italic: false,
-};
+export const textStylesBase = css`
+  line-height: 1.5;
+  ${props => props.italic ? 'text-style: italic;' : ''}
+`;
