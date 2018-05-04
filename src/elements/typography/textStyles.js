dce4c983c1ffcaabc663cdb7d2efe8ce79f2792d
NAPA\respin <respin@napa.com>
2018-05-04 11:44:50 -0400

    Adding default value for fontSizeBase

--- src/elements/typography/textStyles.js
+++ src/elements/typography/textStyles.js
@@ -27,7 +27,7 @@ textStylesBase.propTypes = {
 };
 
 textStylesBase.defaultProps = {
-  fontSizeBase: fontSizeObject.baseFontSize,
+  fontSizeBase: '1rem',
   italic: false,
   lg: PropTypes.bool,
   sm: PropTypes.bool,
