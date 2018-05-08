c2a7fa3c79d2e2b21f8926e50c5969b57a196b8f
NAPA\respin <respin@napa.com>
2018-05-08 13:50:03 -0400

    Adding new styles for List and ListItem

--- src/elements/typography/List.js
+++ src/elements/typography/List.js
@@ -3,5 +3,7 @@ import { textStylesBase } from './textStyles';
 
 export const List = styled.ul`
   ${textStylesBase}
+  margin: 0 0 1rem;
+  padding-left: 2.5rem;
 `;
 
--- src/elements/typography/ListItem.js
+++ src/elements/typography/ListItem.js
@@ -3,5 +3,6 @@ import { textStylesBase } from './textStyles';
 
 export const ListItem = styled.li`
   ${textStylesBase}
+  margin-bottom: 0.25rem;
 `;
 
