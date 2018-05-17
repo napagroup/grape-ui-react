196da78d4ffedc62f94882d5e8487dfe262e8d15
Ryan Espin <respin@napa.com>
2018-05-17 17:21:46 +0000

    Merged in topics/UI-71-make-lg-sm-work-for-text-component (pull request #13)
    Fixed up Text size.
    
    Approved-by: John Shin <jshin@napa.com>

--- src/elements/typography/Text.js
+++ src/elements/typography/Text.js
@@ -1,6 +1,16 @@
+import React from 'react';
 import styled from 'styled-components';
 import { textStylesBase } from './textStyles';
 
-export const Text = styled.span`
-  ${textStylesBase}
-`;
+export const Text = props => {
+  const { ...otherProps } = props;
+  const overrides = {
+    ...otherProps,
+    fontSizeBase: '100%',
+  };
+  const actualBase = textStylesBase(overrides);
+  const ProtoText = styled.span`
+    ${actualBase}
+  `;
+  return <ProtoText {...otherProps} />;
+};
