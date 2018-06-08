4bf2c409c658d400697a5fa3a31e71bcfdbae6e6
John Shin <jshin@napa.com>
2018-06-08 15:19:59 +0000

    Merged in features/styled-components (pull request #19)
    Features/styled components
    
    * Exposed styled-components as part of module exports.
    
    * Export entire styled-component module.
    
    Approved-by: Philip Browning II <pbrowning@napa.com>

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import { Button } from './elements/Button';
 import { grapeHtmlStylesInjector } from './elements/GrapeHtmlStylesInjector';
 import * as Typography from './elements/typography';
 import { getGlobalStyles } from './global-styles';
+import * as styledComponents from 'styled-components';
 
 module.exports = {
   Button,
   getGlobalStyles,
   grapeHtmlStylesInjector,
   ...Typography,
+  styledComponents,
 };
 
