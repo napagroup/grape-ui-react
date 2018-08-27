5d783da3cef4752edcd8a73488c3de178aab860f
Ping Chung <pchung@napa.com>
2018-08-27 13:47:16 -0400

    create build for the form

--- src/elements/form/tests/Form.test.js
+++ src/elements/form/tests/Form.test.js
@@ -5,15 +5,11 @@ import 'jest-styled-components';
 import Adapter from 'enzyme-adapter-react-16';
 import { configure, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import renderer from 'react-test-renderer';
 
 configure({ adapter: new Adapter() });
 
 describe('Form Component base', () => {
   it('should return a Form object without class inline', () => {
-    // const component = renderer.create(<Form />);
-    // const tree = component.toJSON();
-    // expect(tree).toMatchSnapshot();
     const component = mount(<Form />);
     expect(toJson(component.find('Form'))).toMatchSnapshot();
   });
