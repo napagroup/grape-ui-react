a13a474512969db05cb79e02bd2526d191f7ad2c
Ryan Espin <ryanespin@Ryans-MacBook-Pro-2.local>
2018-08-27 16:09:23 -0400

    Added in ControlLabel component.

---
+++ src/elements/form/ControlLabel.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { defaultTextStylesBase } from '../typography/textStyles';
+import { withBaseStyles } from 'src/decorators';
+import { passThrough } from 'src/utils/componentHelpers';
+
+export const ControlLabel = props => {
+  const overrides = {
+    ...props,
+    sm: true,
+  };
+  const ProtoControlLabel = withBaseStyles(styled.label``, overrides);
+  return <ProtoControlLabel {...passThrough(ControlLabel, props)} />;
+};
+
+ControlLabel.propTypes = {
+  color: PropTypes.string,
+  fontFamily: PropTypes.string,
+  fontWeight: PropTypes.string,
+  kerning: PropTypes.string,
+  lg: PropTypes.bool,
+  sm: PropTypes.bool,
+  textAlign: PropTypes.string,
+  textDecoration: PropTypes.string,
+};
+
+ControlLabel.defaultProps = {
+  color: defaultTextStylesBase.color,
+  fontFamily: defaultTextStylesBase.fontFamily,
+  fontWeight: defaultTextStylesBase.fontWeight,
+  kerning: defaultTextStylesBase.kerning,
+  lg: defaultTextStylesBase.lg,
+  sm: defaultTextStylesBase.sm,
+  textAlign: defaultTextStylesBase.textAlign,
+  textDecoration: defaultTextStylesBase.textDecoration,
+};
--- src/elements/form/controlStyles.js
+++ src/elements/form/controlStyles.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { ControlLabel } from './ControlLabel';
 
 const ControlGroup = styled.div``;
-const ControlLabel = styled.label``;
 const AssistiveText = styled.small``;
 const ValidationText = styled.small``;
 
