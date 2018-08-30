f449fa14ae3698f78814ac0d752bfe5851b2fbc7
Ryan Espin <ryanespin@Ryans-MacBook-Pro-2.local>
2018-08-30 15:37:18 -0400

    Updated Form Inline to add spacing between elements.

--- src/elements/form/Form.js
+++ src/elements/form/Form.js
@@ -4,6 +4,12 @@ import styled from 'styled-components';
 import { passThrough } from 'src/utils/componentHelpers';
 import { withStyledSystem } from 'src/decorators';
 import { width, display } from 'styled-system';
+import { getGlobalStyles } from 'src/global-styles';
+import { getScaledSize } from 'src/elements/typography/utils';
+
+const { grid: gridSchema } = getGlobalStyles();
+
+const margin = getScaledSize(gridSchema.gutter, 0.5);
 
 export const Form = props => {
   const { formInline } = props;
@@ -13,7 +19,15 @@ export const Form = props => {
     display: !formInline ? '' : 'flex',
 
   };
-  const ProtoForm = withStyledSystem(styled.form``, overrides);
+  const ProtoForm = withStyledSystem(styled.form`
+      ${!formInline ? '' : `
+        > * {
+          margin: 0 ${margin};
+          &:first-child { margin-left: 0; }
+          &:last-child { margin-right: 0; }
+        }
+      `}
+    `, overrides);
   const propsToPassThru = {
     ...passThrough(Form, overrides),
   };
