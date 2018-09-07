f4f2735b5b65a8aa14f3b5cd4bbb3a0641813f21
Ping Chung <pchung@napa.com>
2018-09-07 17:00:46 -0400

    make the SelectField.test more easy to understandable

--- src/elements/form/SelectField/tests/SelectField.test.js
+++ src/elements/form/SelectField/tests/SelectField.test.js
@@ -43,21 +43,21 @@ describe('SelectField Component with ControlLabel with *', () => {
 });
 
 describe('SelectField Component with style', () => {
-  it('should return a SelectField with style lg prop = true', () => {
+  it('should return a SelectFieldComponent with props where lg={true}', () => {
     const component = mount(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" lg options={colorOptions} value={colorOptions[1]} />);
     expect((component.find('SelectFieldComponent').props('lg'))).toMatchSnapshot();
   });
-  it('should return a SelectField with sm text', () => {
+  it('should return a SelectFieldComponent with props where sm={true}', () => {
     const component = mount(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} sm value={colorOptions[1]} />);
     expect((component.find('SelectFieldComponent').props('sm'))).toMatchSnapshot();
   });
-  it('should return a SelectField with bgColor', () => {
+  it('should return a SelectField with background-color: hsl(4.1,89.6%,58.4%) in c0 and c2 ', () => {
     const component = renderer.create(<SelectField assistiveText="Please tell me your color" bgColor="red" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} value={colorOptions[1]} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  it('should return a SelectField with orange borderColor', () => {
+  it('should return a SelectField with orange borderColor with border: 1px solid orange in c1;', () => {
     const component = renderer.create(<SelectField assistiveText="Please tell me your color" borderColor="orange" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} value={colorOptions[1]} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
@@ -65,7 +65,7 @@ describe('SelectField Component with style', () => {
 });
 
 describe('SelectField Component with space', () => {
-  it('should return a SelectField with space', () => {
+  it('should return a SelectField with space with  margin: 32px;  padding: 32px; on c0', () => {
     const component = renderer.create(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" m={4} options={colorOptions} p={4} value={colorOptions[1]} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
@@ -73,19 +73,19 @@ describe('SelectField Component with space', () => {
 });
 
 describe('SelectField Component with plainText or disabled', () => {
-  it('should return a SelectField with plainText', () => {
+  it('should return a SelectField with plainText with <div> Yellow </div> rendered', () => {
     const component = renderer.create(<SelectField controlId="exampleColor" controlLabel="Color" id="exampleColor" plainText value={colorOptions[1]} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it('should return a SelectField with disabled', () => {
+  it('should return a SelectField with disabled input contains a props "disabled": true', () => {
     const component = mount(<SelectField controlId="exampleColor" controlLabel="Color" disabled id="exampleColor" value={colorOptions[1]} />);
     expect((component.find('input').props('disabled'))).toMatchSnapshot();
   });
 });
 
 describe('SelectField Component with react-select predefined props', () => {
-  it('should return a SelectField with isClearable and placeholder ', () => {
+  it('should return a SelectField with isClearable and placeholder contains a element svg that draw a x', () => {
     const component = mount(<SelectField assistiveText="Please tell me your color, hit x to clear" controlId="exampleColor" controlLabel="Color" id="exampleColor" isSearchable m={4} options={colorOptions} p={4} placeholder="Please search your color" />);
     expect((component.find('svg').props())).toMatchSnapshot();
   });
