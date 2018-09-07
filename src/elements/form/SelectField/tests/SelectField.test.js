ee02e732aaec8ed1266e88059c8904aa4fd8a3d2
Ping Chung <pchung@napa.com>
2018-09-07 16:25:12 -0400

    update the way to do unit test

--- src/elements/form/SelectField/tests/SelectField.test.js
+++ src/elements/form/SelectField/tests/SelectField.test.js
@@ -4,6 +4,7 @@ import 'jest-styled-components';
 import Adapter from 'enzyme-adapter-react-16';
 import { configure, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import renderer from 'react-test-renderer';
 
 configure({ adapter: new Adapter() });
 const colorOptions = [
@@ -13,81 +14,81 @@ const colorOptions = [
 ];
 
 describe('SelectField Component base', () => {
-  it('should return a SelectField object', () => {
+  it('should return a SelectField that contains SelectFieldComponent object', () => {
     const component = mount(<SelectField controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} sm value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
+    expect((component.find('SelectFieldComponent').html())).toMatchSnapshot();
+  });
+  it('should return a SelectField that contains ControlLabel object', () => {
+    const component = mount(<SelectField controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} sm value={colorOptions[1]} />);
+    expect((component.find('ControlLabel').html())).toMatchSnapshot();
   });
 });
 
 describe('SelectField Component with assistive text', () => {
   it('should return a SelectField with assistive text', () => {
     const component = mount(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
-  });
-
-  it('should return a SelectField with required text', () => {
-    const component = mount(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} required value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
+    expect((component.find('AssistiveText').html())).toMatchSnapshot();
   });
 
   it('should return a SelectField with error text', () => {
     const component = mount(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} validationError="This is a required field." value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
+    expect((component.find('AssistiveText').html())).toMatchSnapshot();
   });
 });
 
-describe('SelectField Component with lg text', () => {
-  it('should return a SelectField with style', () => {
-    const component = mount(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" lg options={colorOptions} value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
+describe('SelectField Component with ControlLabel with *', () => {
+  it('should return a SelectField  with ControlLabel with *', () => {
+    const component = mount(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} required value={colorOptions[1]} />);
+    expect((component.find('ControlLabel').html())).toMatchSnapshot();
   });
+});
 
+describe('SelectField Component with style', () => {
+  it('should return a SelectField with style lg prop = true', () => {
+    const component = mount(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" lg options={colorOptions} value={colorOptions[1]} />);
+    expect((component.find('SelectFieldComponent').props('lg'))).toMatchSnapshot();
+  });
   it('should return a SelectField with sm text', () => {
     const component = mount(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} sm value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
+    expect((component.find('SelectFieldComponent').props('sm'))).toMatchSnapshot();
+  });
+  it('should return a SelectField with bgColor', () => {
+    const component = renderer.create(<SelectField assistiveText="Please tell me your color" bgColor="red" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} value={colorOptions[1]} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
   it('should return a SelectField with orange borderColor', () => {
-    const component = mount(<SelectField assistiveText="Please tell me your color" borderColor="orange" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
-  });
-
-  it('should return a SelectField with bgColor', () => {
-    const component = mount(<SelectField assistiveText="Please tell me your color" bgColor="red" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
-  });
-
-  it('should return a SelectField with activeColor', () => {
-    const component = mount(<SelectField activeColor="cyan" assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
+    const component = renderer.create(<SelectField assistiveText="Please tell me your color" borderColor="orange" controlId="exampleColor" controlLabel="Color" id="exampleColor" options={colorOptions} value={colorOptions[1]} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
 
 describe('SelectField Component with space', () => {
   it('should return a SelectField with space', () => {
-    const component = mount(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" m={4} options={colorOptions} p={4} value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
+    const component = renderer.create(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" m={4} options={colorOptions} p={4} value={colorOptions[1]} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
 
 describe('SelectField Component with plainText or disabled', () => {
   it('should return a SelectField with plainText', () => {
-    const component = mount(<SelectField controlId="exampleColor" controlLabel="Color" id="exampleColor" plainText value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
+    const component = renderer.create(<SelectField controlId="exampleColor" controlLabel="Color" id="exampleColor" plainText value={colorOptions[1]} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
   it('should return a SelectField with disabled', () => {
     const component = mount(<SelectField controlId="exampleColor" controlLabel="Color" disabled id="exampleColor" value={colorOptions[1]} />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
+    expect((component.find('input').props('disabled'))).toMatchSnapshot();
+
   });
 });
 
 describe('SelectField Component with react-select predefined props', () => {
-  it('should return a SelectField with isSearchable and placeholder ', () => {
-    const component = mount(<SelectField assistiveText="Please tell me your color" controlId="exampleColor" controlLabel="Color" id="exampleColor" isSearchable m={4} options={colorOptions} p={4} placeholder="Please search your color" />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
-  });
   it('should return a SelectField with isClearable and placeholder ', () => {
     const component = mount(<SelectField assistiveText="Please tell me your color, hit x to clear" controlId="exampleColor" controlLabel="Color" id="exampleColor" isSearchable m={4} options={colorOptions} p={4} placeholder="Please search your color" />);
-    expect(toJson(component.find('SelectField'))).toMatchSnapshot();
+    expect((component.find('svg').props())).toMatchSnapshot();
   });
 });
