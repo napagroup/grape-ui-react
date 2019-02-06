fcead4d5444d4316a7fb5f2518308eaa4a9a1f1b
John Shin <JShin@napa.com>
2019-02-06 16:09:00 -0500

    List updated.

--- src/elements/typography/List.js
+++ src/elements/typography/List.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { getGlobalStyles } from 'src/global-styles';
@@ -12,7 +13,9 @@ import {
   getColor,
   getTextAlign,
   getTextDecoration,
+  typography,
 } from './textStyles';
+import { passThrough } from 'src/utils/componentHelpers';
 
 const { grid: gridSchema } = getGlobalStyles();
 
@@ -31,9 +34,37 @@ const getInLineStyles = props => {
 const getPaddingLeft = props => `padding-left:  ${(props.unstyled || props.inline) ? '0' : '2.5rem'};`;
 const getListStyle = props => `${props.unstyled ? '> li { list-style: none; }' : ''}`;
 
+
 const ListFactory = factoryProps => {
   const { tag } = factoryProps;
-  return styled[tag]`
+  const ListComponent = ({
+    children, className, ...props
+  }) => {
+    if (tag === 'ol') {
+      return (
+        <ol {...passThrough(ListComponent, props)} className={className}>
+          {children}
+        </ol>);
+    }
+    return (
+      <ul {...passThrough(ListComponent, props)} className={className}>
+        {children}
+      </ul>);
+  };
+  ListComponent.propTypes = {
+    children: PropTypes.any.isRequired,
+    className: PropTypes.string,
+    inline: PropTypes.bool,
+    ...typography.propTypes,
+    unstyled: PropTypes.bool,
+  };
+  ListComponent.defaultProps = {
+    className: '',
+    inline: false,
+    unstyled: false,
+  };
+
+  return styled(ListComponent)`
     ${getFontFamily}
     ${getFontSize}
     ${getFontWeight}
@@ -51,20 +82,11 @@ const ListFactory = factoryProps => {
 };
 
 const List = ListFactory({ tag: 'ul' });
-
 List.ul = List;
 List.ol = ListFactory({ tag: 'ol' });
 List.propTypes = {
-  color: PropTypes.string,
-  display: PropTypes.bool,
-  fontFamily: PropTypes.string,
-  fontWeight: PropTypes.string,
   inline: PropTypes.bool,
-  kerning: PropTypes.string,
-  lg: PropTypes.bool,
-  sm: PropTypes.bool,
-  textAlign: PropTypes.string,
-  textDecoration: PropTypes.string,
+  ...typography.propTypes,
   unstyled: PropTypes.bool,
 };
 export { List };
