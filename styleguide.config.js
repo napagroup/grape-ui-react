6bdd7fec7c3dae0a7301979d79caa50177fb0b29
Edwin Vera <evera@napa.com>
2020-05-06 16:26:40 -0400

    removed uncommented code and fix spacing issue

--- styleguide.config.js
+++ styleguide.config.js
@@ -149,9 +149,6 @@ module.exports = {
   skipComponentsWithoutExample: true,
   styleguideComponents: require('./styleguide/containers/index.js'), // eslint-disable-line global-require
   styles: './styleguide/styles.js',
-  // template: {
-  //   favicon: 'data:;base64,iVBORw0KGgo=',
-  // },
   theme: './styleguide/themes.js',
   title: 'Docs | grape-ui',
   tocMode: 'collapse',
