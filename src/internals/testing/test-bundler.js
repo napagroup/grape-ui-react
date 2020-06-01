f5c115e3489d6357d9c9652021616948268f5510
John Shin <jshin@napa.com>
2020-06-01 12:28:33 -0400

    introducing prod_build_job

--- src/internals/testing/test-bundler.js
+++
@@ -1,3 +0,0 @@
-// https://github.com/zloirock/core-js/blob/master/docs/2019-03-19-core-js-3-babel-and-a-look-into-the-future.md#babelpolyfill
-import "core-js/stable";
-import "regenerator-runtime/runtime";
