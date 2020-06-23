5fccf2bb5bddf93a4a4f055d69465724173771d2
John Shin <jshin@napa.com>
2020-06-23 11:12:06 -0400

    Finalized major features for Linear and Circular Progress

---
+++ src/elements/Progress/LinearProgress/index.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { keyframes } from 'styled-components';
+import { Hideable } from 'src/elements/utils';
+import { makeColorResolver } from '../utils';
+
+const backgroundIndicatorColor = makeColorResolver('background', 'indicatorColor');
+const backgroundTrackColor = makeColorResolver('background', 'trackColor');
+
+const determinate1 = keyframes`
+  0% {
+    left: -100%;
+    right: 100%;
+  }
+  50% {
+    left: -50%;
+    right: 100%;
+  }
+  95% {
+    left: -5%;
+    right: 100%;
+  }
+`;
+
+const indeterminate1 = keyframes`
+  0% {
+    left: -35%;
+    right: 100%;
+  }
+  60% {
+    left: 100%;
+    right: -90%;
+  }
+  100% {
+    left: 100%;
+    right: -90%;
+  }
+`;
+
+const StyledRoot = styled('div')`
+  position: relative;
+  overflow: hidden;
+  height: 4;
+  @media print: {
+    colorAdjust: exact;
+  };
+`;
+
+const StyledDiv = styled('div')`
+  height: 5px;
+  position: relative;
+  ${backgroundTrackColor}
+  padding: 1px;
+  box-shadow: inset 0 -1px 1px rgba(255,255,255,0.3);
+`;
+
+const StyledLineDet = styled('span')`
+  width: 100%;
+  position: absolute;
+  ${backgroundIndicatorColor}
+  left: 0;
+  bottom: 0;
+  top: 0;
+  animation: ${determinate1} ${props => props.duration}s cubic-bezier(0.165, 0.84, 0.44, 0.001) ${props => (props.loop ? 'infinite' : '')};
+`;
+
+const StyledLineIndet = styled('span')`
+  width: 100%;
+  position: absolute;
+  ${backgroundIndicatorColor}
+  left: 0;
+  bottom: 0;
+  top: 0;
+  transition: transform 0.2s linear;
+  transformOrigin: left;
+  animation: ${indeterminate1} 1.1s cubic-bezier(0.65, 0.815, 0.735, 0.395) infinite;
+`;
+
+const getTrackColor = props => (props.hideTrack ? 'transparent' : props.trackColor);
+
+export const LinearProgress = props => {
+  const {
+    duration,
+    hideTrack,
+    indicatorColor,
+    isDeterminate,
+    loop,
+    trackColor,
+  } = props;
+  return (
+    <StyledRoot>
+      <StyledDiv trackColor={getTrackColor({ hideTrack, trackColor })}>
+        <Hideable hide={!isDeterminate}>
+          <StyledLineDet
+            duration={duration}
+            indicatorColor={indicatorColor}
+            loop={loop}
+          />
+        </Hideable>
+        <Hideable hide={isDeterminate}>
+          <StyledLineIndet
+            indicatorColor={indicatorColor}
+            loop={loop}
+          />
+        </Hideable>
+      </StyledDiv>
+    </StyledRoot>
+  );
+};
+LinearProgress.propTypes = {
+  duration: PropTypes.number,
+  hideTrack: PropTypes.bool,
+  indicatorColor: PropTypes.string,
+  isDeterminate: PropTypes.bool,
+  loop: PropTypes.bool,
+  trackColor: PropTypes.string,
+};
+
+LinearProgress.defaultProps = {
+  duration: 25,
+  hideTrack: false,
+  indicatorColor: 'indigo.dark',
+  isDeterminate: false,
+  loop: true,
+  trackColor: 'indigo.light',
+};
--- src/elements/Progress/styled.js
+++ src/elements/Progress/styled.js
@@ -1,161 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { keyframes } from 'styled-components';
-import { resolveColor } from 'src/utils/styledHelpers';
 import { Hideable } from 'src/elements/utils';
-
-const circularRotate = keyframes`
-from {
-  transform: rotate(0deg);
-}
-
-to {
-  transform: rotate(360deg);
-}
-`;
-
-const circularDash = props => keyframes`
-from {
-  stroke-dashoffset: ${props.circumference};
-}
-to {
-  stroke-dashoffset: ${-1 * props.circumference};
-}
-`;
-
-const StyledDiv = styled('div')`
-  animation: ${circularRotate} 2.4s linear infinite;
-`;
-
-const StyledSvg = styled('svg')`
-  display: block;
-  margin: 20px auto;
-  max-width: 100%;
-
-`;
-
-const StyledCircleBg = styled('circle')`
-  fill: none;
-  stroke: ${props => resolveColor(props.strokeColor)};
-`;
-
-const StyledCircleFg = styled('circle')`
-  animation: ${circularDash} 2.4s ease-in-out infinite;
-  fill: none;
-  stroke: ${props => resolveColor(props.strokeColor)};
-`;
-
-const StyledDivDet = styled('div')`
-  transform: rotate(-90deg);
-`;
-
-const determinateCircularDash = props => keyframes`
-from {
-  stroke-dashoffset: ${props.circumference};
-}
-to {
-  stroke-dashoffset: ${0};
-}
-`;
-
-const StyledDeterminateCircleFg = styled('circle')`
-  animation: ${determinateCircularDash} 4.2s ease-in-out infinite;
-  fill: none;
-  stroke: ${props => resolveColor(props.strokeColor)};
-`;
-
-const CircularProgress = props => {
-  const {
-    circleBgStroke,
-    circleFgStroke,
-    hideTrack,
-    isDeterminate,
-    size,
-    strokeWidth,
-  } = props;
-  const radius = size / 2 - strokeWidth / 2;
-  const circumference = 2 * Math.PI * radius;
-  const center = size / 2;
-  return (
-    <>
-      <Hideable hide={!isDeterminate}>
-        <StyledDivDet>
-          <StyledSvg className="svg" height={size} width={size}>
-            <Hideable hide={hideTrack}>
-              <StyledCircleBg
-                circumference={circumference}
-                cx={center}
-                cy={center}
-                r={radius}
-                strokeColor={circleBgStroke}
-                strokeWidth={strokeWidth}
-              />
-            </Hideable>
-            <StyledDeterminateCircleFg
-              circumference={circumference}
-              cx={center}
-              cy={center}
-              r={radius}
-              strokeColor={circleFgStroke}
-              strokeDasharray={circumference}
-              strokeWidth={strokeWidth}
-            />
-          </StyledSvg>
-        </StyledDivDet>
-      </Hideable>
-      <Hideable hide={isDeterminate}>
-        <StyledDiv>
-          <StyledSvg className="svg" height={size} width={size}>
-            <Hideable hide={hideTrack}>
-              <StyledCircleBg
-                circumference={circumference}
-                cx={center}
-                cy={center}
-                r={radius}
-                strokeColor={circleBgStroke}
-                strokeWidth={strokeWidth}
-              />
-            </Hideable>
-            <StyledCircleFg
-              circumference={circumference}
-              cx={center}
-              cy={center}
-              r={radius}
-              strokeColor={circleFgStroke}
-              strokeDasharray={circumference}
-              strokeWidth={strokeWidth}
-            />
-          </StyledSvg>
-        </StyledDiv>
-      </Hideable>
-    </>
-  );
-};
-
-CircularProgress.propTypes = {
-  circleBgStroke: PropTypes.string,
-  circleFgStroke: PropTypes.string,
-  hideTrack: PropTypes.bool,
-  isDeterminate: PropTypes.bool,
-  size: PropTypes.number,
-  strokeWidth: PropTypes.number,
-};
-
-CircularProgress.defaultProps = {
-  circleBgStroke: 'indigo.light',
-  circleFgStroke: 'indigo.dark',
-  hideTrack: false,
-  isDeterminate: false,
-  size: 35,
-  strokeWidth: 5,
-};
+import { CircularProgress } from './CircleProgress';
+import { LinearProgress } from './LinearProgress';
 
 const showCircular = props => props.progressType === 'circular';
+const showLinear = props => props.progressType === 'linear';
 
 export const Progress = props => (
   <>
     <Hideable hide={!showCircular(props)}>
       <CircularProgress {...props} />
+    </Hideable>
+    <Hideable hide={!showLinear(props)}>
+      <LinearProgress {...props} />
     </Hideable>
   </>
 );
