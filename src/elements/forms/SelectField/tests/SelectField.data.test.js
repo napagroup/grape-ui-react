3cdfef52497e9e4467623ab7adc1dbca2d338bdb
John Shin <jshin@napa.com>
2020-07-16 15:49:45 -0400

    Added form data tests for SelectField, TextField

--- src/elements/forms/SelectField/tests/SelectField.data.test.js
+++ src/elements/forms/SelectField/tests/SelectField.data.test.js
@@ -5,18 +5,49 @@ import {
 } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
+import selectEvent from 'react-select-event';
 import {
   Controller,
   useForm,
 } from 'react-hook-form';
 import { Button } from 'src/elements/Button';
 import { Form } from 'src/elements/forms';
-import { DateField } from '..';
+import { SelectField } from '..';
 
 const buttonText = 'Submit form';
+const labelText = 'Courses';
 const defaultValues = {
-  enrollmentDate: new Date('2020-07-14'),
+  courses: {
+    label: '🎨 Arts & Humanities',
+    value: 'artsHumanities',
+  },
 };
+const courseOptions = [
+  {
+    label: '🎨 Arts & Humanities',
+    value: 'artsHumanities',
+  },
+  {
+    label: '👔 Business',
+    value: 'business',
+  },
+  {
+    label: '🤖 Artificial Intelligence',
+    value: 'artificialIntelligence',
+  },
+  {
+    label: '🤸‍♀️ Health',
+    value: 'health',
+  },
+  {
+    label: '💃 Music & Dance',
+    value: 'musicDance',
+  },
+  {
+    label: '🌎 Language Learning',
+    value: 'languageLearning',
+  },
+];
 function App({ defaultFormData, getFormData }) {
   const {
     control,
@@ -27,13 +58,13 @@ function App({ defaultFormData, getFormData }) {
     <Form role="form">
       <Controller
         control={control}
-        name="enrollmentDate"
-        render={({ onChange }) => (
-          <DateField
-            labelText="Enrollment Date"
-            locale="it"
-            name="enrollmentDate"
-            onChange={selected => onChange(selected.selectedDay)}
+        name="courses"
+        render={({ onChange, value }) => (
+          <SelectField
+            labelText={labelText}
+            name="courses"
+            onChange={onChange}
+            options={courseOptions}
           />
         )}
       />
@@ -48,7 +79,7 @@ function App({ defaultFormData, getFormData }) {
   );
 }
 
-describe('DateField - data', () => {
+describe('SelectField - data', () => {
   let getFormData;
 
   beforeEach(() => {
@@ -63,12 +94,13 @@ describe('DateField - data', () => {
     const actual = getFormData.mock.calls[0][0];
     expect(actual).toEqual(defaultValues);
   });
-  it('should have updated value when a new date is selected', () => {
-    fireEvent.click(screen.getByPlaceholderText('M/D/YYYY'));
-    const cells = screen.getAllByRole('gridcell');
-    fireEvent.click(cells[30]);
+  it('should have updated value when an option is selected', async () => {
+    const expected = {
+      courses: courseOptions[1],
+    };
+    await selectEvent.select(screen.getByLabelText(labelText), [courseOptions[1].label]);
     fireEvent.click(screen.getByText(buttonText));
     const actual = getFormData.mock.calls[0][0];
-    expect(actual.enrollmentDate.getDate()).toEqual(31);
+    expect(actual).toEqual(expected);
   });
 });
